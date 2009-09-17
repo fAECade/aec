c0f90f80deeb2b3a42274a30c45349d11a1e42ac
snusnu <gamsnjaga@gmail.com>
2009-09-17 23:48:31 +0200

    Savepoint for work on screen

---
+++ lib/aec/display/pixel.rb
@@ -0,0 +1,13 @@
+module FAECade
+  module Display
+
+    class Pixel
+
+      def initialize(x, y, r, g, b)
+        @x, @y, @r, @g, @b = x, y, r, g, b
+      end
+
+    end
+
+  end
+end
--- lib/aec/display/screen.rb
+++ lib/aec/display/screen.rb
@@ -3,9 +3,33 @@ module FAECade
 
     class Screen
 
-      def initialize(wall)
-        @wall = wall
+      def initialize(wall, root_x, root_y, area)
+        @wall,@root_x, @root_y = wall, root_x, root_y
+        @area = area
         @wall.register(self)
+      end
+
+      def [](x,y)
+
+      end
+
+      private
+
+      #       3,4
+      #     2,3,4,5
+      #   1,2,3,4,5,6
+      # 0,1,2,3,4,7,8,9
+      #   1,2,3,4,5,6
+      #     2,3,4,5
+      #       3,4
+      def init_virtual_area
+        width = @dimensions.map { |line| line.first }.max
+        @virtual_area = [][]
+        @dimensions.each do |line|
+          (O..width).each do |col|
+            @virtual_area[line][col] = (col == line[col]) ? col : nil
+          end
+        end
       end
 
     end
